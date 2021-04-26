8738b377bfab305b9a3fa95811b69396cab39eb3
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2021-04-26 21:18:26 +0000

    update main.py
    we don't need to specipy a instant. In order to automate the execution, i get the actual datetime and i send the message right after that

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import pywhatkit as kit
 import time
 import datetime
 
-
-#choose a sending time and convert to seconds
-instant = 12500
-
-
 date = datetime.datetime.now
 initial = 3600*date.hour + 60 *date.minute
 instant = initial + 60
